61697dc73249a1c261634b4e6959ea06c3ab4019
Denis2982 <denisdenis2982@gmail.com>
2023-03-18 20:03:40 +0200

    init repository and added files

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+
+    $('.nav__burger').on('click', function() {
+      $('.nav__list').toggleClass('nav__list--open');
+      $('.nav__burger').toggleClass('nav__burger--open');
+    });
+
+    $('.footer__burger').on('click', function() {
+      $('.footer__btn').toggleClass('footer__btn--active');
+      $('.footer__menu').toggleClass('footer__menu--active');
+    });
+
+    $('.theme').on('click', function() {
+      $('.theme').toggleClass('theme--night');
+      $('body').toggleClass('body--night');
+      
+     
+    });
+
+})
+
